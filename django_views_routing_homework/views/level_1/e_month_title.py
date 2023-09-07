48d8900030200c5200b7ebceb105346b9648fc5d
Ilya Lebedev <melevir@gmail.com>
2023-09-07 11:53:48 +0400

    fix typos

--- django_views_routing_homework/views/level_1/e_month_title.py
+++ django_views_routing_homework/views/level_1/e_month_title.py
@@ -2,14 +2,14 @@ from django.http import HttpResponse, HttpResponseNotFound
 
 
 """
-Вьюха get_user_info возвращает название месяца по его номеру. 
+Вьюха get_month_title_view возвращает название месяца по его номеру. 
 Вся логика работы должна происходить в функции get_month_title_by_number.
 
 Задания:
     1. Напишите логику получения названия месяца по его номеру в функции get_month_title_by_number
     2. Если месяца по номеру нет, то должен возвращаться ответ типа HttpResponseNotFound c любым сообщением об ошибке
     3. Добавьте путь в файле urls.py, чтобы при открытии http://127.0.0.1:8000/month-title/тут номер месяца/ 
-       вызывалась вьюха get_month_title_by_number. Например http://127.0.0.1:8000/month-title/3/ 
+       вызывалась вьюха get_month_title_view. Например http://127.0.0.1:8000/month-title/3/ 
 """
 
 
